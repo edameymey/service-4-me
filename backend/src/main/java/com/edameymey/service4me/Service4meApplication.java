8ef520f4f1df1dcbc8e281738799fecb01ebd49b
dragancristian2005 <cristiandragan34@yahoo.ro>
2026-03-13 19:44:06 +0200

    initial setup

---
+++ backend/src/main/java/com/edameymey/service4me/Service4meApplication.java
@@ -0,0 +1,13 @@
+package com.edameymey.service4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service4meApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service4meApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edameymey/service4me/Service4meApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edameymey.service4me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service4meApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
